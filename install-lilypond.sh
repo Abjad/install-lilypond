b0cfc23468b58dc594e3836e4dd4451a788d6a1f
Trevor Bača <trevor.baca@gmail.com>
2025-01-29 16:18:22 -0500

    Bump LilyPond from 2.25.22 to 2.25.23

--- install-lilypond.sh
+++ install-lilypond.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 set -e
 
-LILYPOND_VERSION=2.25.22
-CHECKSUM="3e837e811aaa72323863925f0a4b5bd1aff1ed9b997e6c70dbe5c008e47872e7"
+LILYPOND_VERSION=2.25.23
+CHECKSUM="9ff92d90f6af647e25bdc9b94ee2716ad9d9d58d7d48bd4184b7f488c3f27535"
 
 echo "Installing LilyPond version: ${LILYPOND_VERSION}"
 lilypond_archive="lilypond-${LILYPOND_VERSION}-linux-x86_64.tar.gz"
